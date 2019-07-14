a194c1284ce3de34c4b3e61756285828215edd03
Sangin Kang <sangin84@gmail.com>
2019-07-15 07:23:18 +0900

    First commit

---
+++ android/app/src/main/kotlin/flutteranimation3/bearcola/com/flutter_animation3/MainActivity.kt
@@ -0,0 +1,13 @@
+package flutteranimation3.bearcola.com.flutter_animation3
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
